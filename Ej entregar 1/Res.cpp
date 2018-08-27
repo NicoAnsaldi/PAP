ddeabe11c1134a90b89000a7bf036fd317ee46f2
Nicolas Adolfo Ansaldi <nansaldi@ws6.labo2>
2018-08-27 16:41:22 -0300

    Incio de ej1

---
+++ Ej entregar 1/Res.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void mostrarVector(vector<int>&);
+vector<int> PD(int, int, vector<int>&, int);
+
+int main(){
+	int cantCases = 0;
+	int ferryLarge = 0;
+	cin >> cantCases;
+	
+	while(cantCases > 0){
+		cin >> ferryLarge;
+		vector<int> cars;
+		vector<int> res;
+		int aux = 1;
+		cin >> aux;
+		
+		while(aux > 0){
+			cars.push_back(aux);
+			cin >> aux;
+		}
+
+		res = PD(ferryLarge, ferryLarge, cars, 0);
+		cout << endl;
+		cantCases --;
+	}
+
+
+
+
+
+	return 0;
+}
+
+
+
+vector<int> PD(int ferryD, int ferryI, vector<int>& cars, int i){
+	vector<int> res;
+	if(cars[i] > ferryD && cars[i] > ferryI){
+		return 0;
+	}
+
+	
+
+
+	return res;
+}
+
+
+
+void mostrarVector(vector<int>& x){
+	int i = 0;
+	while(i < x.size()){
+		cout << x[i];
+		i++;
+	}
+}
