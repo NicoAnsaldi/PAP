b49b582198cf5f035b0b24aff3e1a925d0502a12
Nicolas Adolfo Ansaldi <nansaldi@ws8.labo4>
2018-09-17 20:10:43 -0300

    No entiendo nada

--- 2D Nim/Res.cpp
+++ 2D Nim/Res.cpp
@@ -9,18 +9,21 @@ int height = 0;
 int pieces = 0;
 
 void mostrarMatriz(vector< vector< pair<int,int> > >& matriz);
-void DFS(vector< vector<int> >& gird, vector< vector<int> >& matrizColoreo);
+int DFS(vector< vector< pair<int,int> > >& grid, int i, int j);
+void vectoresIguales(vector<int>& v1, vector<int>& v2);
+void mostrarVector(vector<int>& v);
 
 int main(){
 	int cases;
 	cin >> cases;
 	
-	for(int i = 0; i < cases; i++){
+	int i = 0;
+	while(i < cases){
 		cin >> width;
 		cin >> height;
 		cin >> pieces;
 
-		vector< vector< pair<int,int> > > grid1(width, vector< pair<int,int> >(height,pair<int,int>(0,-1)));
+		vector< vector< pair<int,int> > > grid1(width, vector< pair<int,int> >(height,pair<int,int>(0,0)));
 		for(int j = 0; j < pieces; j++){
 			int x = 0;
 			int y = 0;
@@ -28,20 +31,103 @@ int main(){
 			grid1[x][y].first = 1;
 		}
 
-		 mostrarMatriz(grid1);
-		vector< vector< pair<int,int> > > grid2 (width, vector< pair<int,int> >(height,pair<int,int>(0,-1)));
+		//mostrarMatriz(grid1);
+		vector< vector< pair<int,int> > > grid2 (width, vector< pair<int,int> >(height,pair<int,int>(0,0)));
 		for(int j = 0; j < pieces; j++){
 			int x = 0;
 			int y = 0;
 			cin >> x >> y;
 			grid2[x][y].first = 1;
 		}
-		mostrarMatriz(grid2);
+		//mostrarMatriz(grid2);
+		vector<int> clusters1;
+		for(int x = 0; x < width; x++){
+			for(int y = 0; y < height; y++){
+				if(grid1[x][y].first == 1 && grid1[x][y].second == 0){
+					grid1[x][y].second == 1;
+					int res = DFS(grid1, x, y)+1;
+					clusters1.push_back(res);
+				}
+			}
+		}
+		
+		vector<int> clusters2;
+		for(int x = 0; x < width; x++){
+			for(int y = 0; y < height; y++){
+				if(grid2[x][y].first == 1 && grid2[x][y].second == 0){
+					grid2[x][y].second == 1;
+					int res = DFS(grid2, x, y)+1;
+					clusters2.push_back(res);
+				}
+			}
+		}
+		
+		vectoresIguales(clusters1, clusters2);
+		i++;
+		if(i < cases){
+			cout << endl;
+		}
 	}
 
 	return 0;
 }
 
+int DFS(vector< vector< pair<int,int> > >& grid, int i, int j){
+	if(i == height && j == width){
+		return 0;
+	}
+
+	int left = 0;
+	int right = 0;
+	int up = 0;
+	int down = 0;
+	
+	if(i < width-1 && grid[i+1][j].first == 1 && grid[i+1][j].second == 0){
+		grid[i+1][j].second = 1;
+		down = DFS(grid, i+1, j)+1;
+	}
+	
+	if(j < height-1 && grid[i][j+1].first == 1 && grid[i][j+1].second == 0 ){
+		grid[i][j+1].second = 1;
+		right = DFS(grid, i, j+1)+1;
+	}
+	
+	if(j > 0 && grid[i][j-1].first == 1 && grid[i][j-1].second == 0){
+		grid[i][j-1].second = 1;
+		left = DFS(grid, i, j-1)+1;
+	}
+	
+	if(i > 0 && grid[i-1][j].first == 1 && grid[i-1][j].second == 0){
+		grid[i-1][j].second = 1;
+		up = DFS(grid, i-1, j)+1;
+	}
+
+	return (left+right+up+down);		
+}
+
+void vectoresIguales(vector<int>& v1, vector<int>& v2){
+	for(int i = 0; i < v1.size(); i++){
+		int elementoABuscar = v1[i];
+		int j = 0;
+		bool flag = true;
+
+		while(j < v2.size()){
+			if(v2[j] == elementoABuscar){
+				v2.erase(v2.begin()+j);
+				flag = false;
+				break;
+			}
+			j++;
+		}
+		
+		if(flag){
+			cout << "NO";
+			return;
+		}
+
+	}
+	cout << "YES";
+}
 
 void mostrarMatriz(vector< vector< pair<int,int> > >& matriz){
 
@@ -52,4 +138,11 @@ void mostrarMatriz(vector< vector< pair<int,int> > >& matriz){
 		cout << endl;
 	}
 	cout << endl << endl;
+}
+
+void mostrarVector(vector<int>& v){
+	for(int i = 0; i < v.size(); i++){
+		cout << v[i];
+	}
+	cout << endl;
 }
