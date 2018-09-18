bc993af46ad4b7cbd7a09dc46c75f493378fbd85
Nicolas Adolfo Ansaldi <nansaldi@ws8.labo4>
2018-09-17 21:17:02 -0300

    La concha de tu madre all boys

--- 2D Nim/Res.cpp
+++ 2D Nim/Res.cpp
@@ -9,7 +9,7 @@ int height = 0;
 int pieces = 0;
 
 void mostrarMatriz(vector< vector< pair<int,int> > >& matriz);
-int DFS(vector< vector< pair<int,int> > >& grid, int i, int j);
+int DFS(vector< vector< pair<int,int> > >& grid, int i, int j, int movimiento);
 void vectoresIguales(vector<int>& v1, vector<int>& v2);
 void mostrarVector(vector<int>& v);
 
@@ -44,9 +44,24 @@ int main(){
 		for(int x = 0; x < width; x++){
 			for(int y = 0; y < height; y++){
 				if(grid1[x][y].first == 1 && grid1[x][y].second == 0){
+					if(((x > 0 && grid1[x-1][y].first == 0) || x == 0)){
+						int res = DFS(grid1, x, y,0);
+						clusters1.push_back(res);
+					}
+					if(((x < width-1 && grid1[x+1][y].first == 0) || x == width-1)){
+						int res = DFS(grid1, x, y,3);
+						clusters1.push_back(res);
+					}
+					if(((y > 0 && grid1[x][y-1].first == 0) || y == 0)){
+						int res = DFS(grid1, x, y,1);
+						clusters1.push_back(res);
+					}
+					if(((y < height-1 && grid1[x][y+1].first == 0) || y == height-1)){
+						int res = DFS(grid1, x, y,2);
+						clusters1.push_back(res);
+					}
+					
 					grid1[x][y].second == 1;
-					int res = DFS(grid1, x, y)+1;
-					clusters1.push_back(res);
 				}
 			}
 		}
@@ -55,24 +70,38 @@ int main(){
 		for(int x = 0; x < width; x++){
 			for(int y = 0; y < height; y++){
 				if(grid2[x][y].first == 1 && grid2[x][y].second == 0){
+					if(((x > 0 && grid2[x-1][y].first == 0) || x == 0)){
+						int res = DFS(grid2, x, y,0);
+						clusters2.push_back(res);
+					}
+					if(((x < width-1 && grid2[x+1][y].first == 0) || x == width-1)){
+						int res = DFS(grid2, x, y,3);
+						clusters2.push_back(res);
+					}
+					if(((y > 0 && grid2[x][y-1].first == 0) || y == 0)){
+						int res = DFS(grid2, x, y,1);
+						clusters2.push_back(res);
+					}
+					if(((y < height-1 && grid2[x][y+1].first == 0) || y == height-1)){
+						int res = DFS(grid2, x, y,2);
+						clusters2.push_back(res);
+					}
+					
 					grid2[x][y].second == 1;
-					int res = DFS(grid2, x, y)+1;
-					clusters2.push_back(res);
 				}
 			}
 		}
 		
 		vectoresIguales(clusters1, clusters2);
 		i++;
-		if(i < cases){
+		//if(i < cases){
 			cout << endl;
-		}
+		//}
 	}
-
 	return 0;
 }
 
-int DFS(vector< vector< pair<int,int> > >& grid, int i, int j){
+int DFS(vector< vector< pair<int,int> > >& grid, int i, int j, int movimiento){
 	if(i == height && j == width){
 		return 0;
 	}
@@ -82,24 +111,21 @@ int DFS(vector< vector< pair<int,int> > >& grid, int i, int j){
 	int up = 0;
 	int down = 0;
 	
-	if(i < width-1 && grid[i+1][j].first == 1 && grid[i+1][j].second == 0){
-		grid[i+1][j].second = 1;
-		down = DFS(grid, i+1, j)+1;
+
+	if(i < width-1 && grid[i+1][j].first == 1 && movimiento == 0){
+		right = DFS(grid, i+1, j,0)+1;
 	}
 	
-	if(j < height-1 && grid[i][j+1].first == 1 && grid[i][j+1].second == 0 ){
-		grid[i][j+1].second = 1;
-		right = DFS(grid, i, j+1)+1;
+	if(j < height-1 && grid[i][j+1].first == 1  && movimiento == 1){
+		down = DFS(grid, i, j+1,1)+1;
 	}
 	
-	if(j > 0 && grid[i][j-1].first == 1 && grid[i][j-1].second == 0){
-		grid[i][j-1].second = 1;
-		left = DFS(grid, i, j-1)+1;
+	if(j > 0 && grid[i][j-1].first == 1 && movimiento == 2){
+		up = DFS(grid, i, j-1,2)+1;
 	}
 	
-	if(i > 0 && grid[i-1][j].first == 1 && grid[i-1][j].second == 0){
-		grid[i-1][j].second = 1;
-		up = DFS(grid, i-1, j)+1;
+	if(i > 0 && grid[i-1][j].first == 1 && movimiento == 3){
+		left = DFS(grid, i-1, j,3)+1;
 	}
 
 	return (left+right+up+down);		
